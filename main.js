4b71ea6e14097813d929d8d5617a9d1ebbbefd84
haniamemon <hania.memon@torontomu.ca>
2024-02-26 16:41:37 -0500

    Update main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+//make a function for when the user clicks the button
+function clickFuncition() {
+  // the prompt () function takes input, which is stored in the username var
+  var username = prompt("Hi there! What's your name?");
+  
+  // use an if else statement to see what the input was, determine output
+  if (username == null || username == "") 
+  {
+   // for no input/error, no name is given
+    document.getElementById("namegiven").innerHTML = "Hello! Nice to meet you!";
+      
+  } else {
+    // for name given, use in output
+    document.getElementById("namegiven").innerHTML = "Hello " +username +"! Nice to meet you!";   
+  }
+        } 
+  
